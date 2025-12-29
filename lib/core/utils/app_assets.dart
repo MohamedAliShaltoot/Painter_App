64e110352ce906e5aec577fb614a4e25a1831c33
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2025-12-29 23:12:31 +0200

    add new app constants trings

--- lib/core/utils/app_assets.dart
+++ lib/core/utils/app_assets.dart
@@ -1,3 +1,8 @@
 abstract class AppStrings {
   static const String appName = 'Painter App';
+  static const String typeHere = "Type here";
+  static const String cancel = 'Cancel';
+  static const String addText = 'Add Text';
+  static const String fontSize = 'Font Size :';
+  static const String textColor = 'Text Color :';
 }
