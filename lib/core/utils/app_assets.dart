bbc88befd1f9fee22dd8aeea9f1ba0ca92bc8052
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2025-12-29 23:50:31 +0200

    add new app constants srings

--- lib/core/utils/app_assets.dart
+++ lib/core/utils/app_assets.dart
@@ -5,4 +5,14 @@ abstract class AppStrings {
   static const String addText = 'Add Text';
   static const String fontSize = 'Font Size :';
   static const String textColor = 'Text Color :';
+  static const String undoToolTip = 'Undo';
+  static const String redoToolTip = 'Redo';
+  static const String clearCanvasToolTip = 'Clear Canvas';
+  static const String addNewPageToolTip = 'Add New Page';
+  static const String deleteCurrentPageToolTip = 'Delete Current Page';
+  static const String toggleThemeToolTip = 'Toggle Theme';
+static const String hideControlsToolTip = 'Hide Controls';
+static const String showControlsToolTip = 'Show Controls';
+static const String appMenuToolTip = 'App Menu';
+  //'Hide Controls'
 }
